be2a529be6856f1800221e9912bc71adad0ba5d9
ItsAchance <alexander.chance@tutanota.com>
2025-08-19 21:24:24 +0200

    Done with more exersices

--- course/4-structs/exercises/5-methods/code.go
+++ course/4-structs/exercises/5-methods/code.go
@@ -8,6 +8,9 @@ type authenticationInfo struct {
 }
 
 // ?
+func (a authenticationInfo) getBasicAuth() string {
+	return fmt.Sprint("Authorization: Basic ", a.username + ":" + a.password)
+}
 
 // don't touch below this line
 
